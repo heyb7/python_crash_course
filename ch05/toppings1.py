a3d45e37bf9406de992abbef537f3c7166c29163
hyb7 <heyanbing7@gmail.com>
2018-01-16 23:17:20 +0800

    chapter 5

--- ch05/toppings1.py
+++ ch05/toppings1.py
@@ -14,3 +14,13 @@ for requested_topping in requested_toppings:
         print("Adding " + requested_topping + ".")
 
 print("\nFinished making your pizza!")
+
+print("\n")
+
+requested_toppings = []
+if requested_toppings:
+    for requested_topping in requested_toppings:
+        print("Adding " + requested_topping + ".")
+    print("\nFinished making your pizza!")
+else:
+    print("Are you sure you want a plain pizza?")
