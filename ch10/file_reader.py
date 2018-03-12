e1b377293258044cac6daa46adb286def2d05d12
heyanbing <heyanbing@emindsoft.com.cn>
2018-03-12 17:58:08 +0800

    chapter 10

---
+++ ch10/file_reader.py
@@ -0,0 +1,18 @@
+#读取整个文件
+with open("D:\git_project\python_crash_course\ch10\pi_digits.txt") as file_object:
+    contents = file_object.read()
+    print(contents)
+
+#逐行读取
+print("\n")
+filename = "D:\git_project\python_crash_course\ch10\pi_digits.txt"
+with open(filename) as file_object:
+    for line in file_object:
+        print(line.rstrip())
+
+print("\n")
+with open(filename) as file_object:
+    lines = file_object.readlines()
+
+for line in lines:
+    print(line.rstrip())
